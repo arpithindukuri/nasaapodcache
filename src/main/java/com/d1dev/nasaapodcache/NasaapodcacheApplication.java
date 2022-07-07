43f657ed017c3f6994cdf9592a9455aa5ad50313
arpithindukuri <danielarpith@gmail.com>
2022-07-06 20:04:54 -0600

    first commit

---
+++ src/main/java/com/d1dev/nasaapodcache/NasaapodcacheApplication.java
@@ -0,0 +1,13 @@
+package com.d1dev.nasaapodcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaapodcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaapodcacheApplication.class, args);
+	}
+
+}
